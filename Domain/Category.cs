03badce1b15a8ce138b40b70452af5986d75629d
jetlirapopaj <jp48443@ubt-uni.net>
2023-02-03 23:53:25 +0100

    category added

---
+++ Domain/Category.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+
+namespace Domain
+{
+    public class Category
+    {
+       
+       public int Id{get;set;}
+        public string Name { get; set; }
+
+        public ICollection<Quiz> Quizzes{get; set;} = new List<Quiz>();
+        
+    }
+}
