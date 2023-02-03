c088b65f51e39eeb85b1b5e9edd95c56a3551f2a
jetlirapopaj <jp48443@ubt-uni.net>
2023-02-03 23:47:22 +0100

    added api logic

---
+++ API/Extensions/ApplicationServerExtensions.cs
@@ -0,0 +1,38 @@
+using System.Collections.Generic;
+using Application.Core;
+using Application.Quizzes;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+using Persistence;
+
+namespace API.Extensions
+{
+    public static class ApplicationServerExtensions
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services,
+        IConfiguration config)
+        {
+             services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
+            });
+            services.AddDbContext<DataContext>(opt =>
+            {
+                object p = opt.UseSqlite(config.GetConnectionString("DefaultConnection"));
+            });
+            services.AddCors(opt =>
+            {
+                opt.AddPolicy("CorsPolicy", policy =>
+                {
+                 policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+                });
+            });
+            services.AddMediatR(typeof(List.Handler).Assembly);
+            services.AddAutoMapper(typeof(MappingProfiles).Assembly);
+            return services;
+        }
+    }
+}
