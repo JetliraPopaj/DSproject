fd8be258c4a217a853897757c8b5896be7e8b768
jetlirapopaj <jp48443@ubt-uni.net>
2023-02-04 21:43:23 +0100

    account controller methods for login and register done

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -16,16 +16,65 @@ namespace API.Controllers
     public class AccountController : ControllerBase
     {
         private readonly UserManager<Player> _userManager;
-        
+        private readonly SignInManager<Player> _signInManager;
         private readonly TokenService _tokenService;
 
         public AccountController(UserManager<Player> userManager,
         SignInManager<Player> signInManager, TokenService tokenService)
         {
             _tokenService = tokenService;
+            _signInManager = signInManager;
             _userManager = userManager;
         }
-        
+        [HttpPost("login")]
+        public async Task<ActionResult<UserDTO>> Login(LoginDTO loginDTO)
+        {
+            var user = await _userManager.FindByEmailAsync(loginDTO.Email);
+
+            if (user == null) return Unauthorized();
+
+            var result = await _signInManager.CheckPasswordSignInAsync(user, loginDTO.Password, false);
+
+            if (result.Succeeded)
+            {
+                return new UserDTO
+                {
+                    DisplayName = user.DisplayName,
+                    Image = null,
+                    Token = _tokenService.CreateToken(user),
+                    Username = user.UserName
+                };
+            }
+            return Unauthorized();
+        }
+        [HttpPost("register")]
+        public async Task<ActionResult<UserDTO>> Register(RegisterDTO registerDto)
+        {
+            if (await _userManager.Users.AnyAsync(x => x.Email == registerDto.Email))
+            {
+                return BadRequest("Email taken");
+            }
+            if(await _userManager.Users.AnyAsync(x => x.UserName == registerDto.UserName))
+            {
+                return BadRequest("Username taken");
+            }
+            var user = new Player
+            {
+                DisplayName = registerDto.DisplayName,
+                Email = registerDto.Email,
+                UserName = registerDto.UserName
+            
+            };
+            var result = await _userManager.CreateAsync(user, registerDto.Password);
+
+            if(result.Succeeded)
+            {
+                return CreatedUserObject(user);
+               
+            }
+
+            return BadRequest("Problem registering user");
+        }
         [HttpGet]
         public async Task<ActionResult<UserDTO>> GetCurrentUser()
         {
