843044b8fadb25f7fb4639fe2bd70b6bbac07203
elmediNahmeti <ea52865@ubt-uni.net>
2023-02-05 23:02:04 +0100

    mapping profiles

---
+++ Application/Core/MappingProfiles.cs
@@ -0,0 +1,17 @@
+using AutoMapper;
+using Domain;
+
+namespace Application.Core
+{
+    public class MappingProfiles : Profile
+    {
+        public MappingProfiles()
+        {
+            CreateMap<Quiz, Quiz>();
+            CreateMap<Question, Question>();
+            CreateMap<Category, Category>();
+            
+
+        }
+    }
+}
