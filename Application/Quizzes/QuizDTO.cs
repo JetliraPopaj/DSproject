dd87f46b27db10c0e4fd3938bb79c6c5e9c9beea
diellzapopaj <dp48842@ubt-uni.net>
2023-02-05 20:58:12 +0100

    quizDTO

---
+++ Application/Quizzes/QuizDTO.cs
@@ -0,0 +1,14 @@
+namespace Application.Quizzes
+{
+    public class QuizDTO
+    {
+        
+        public int Id {get;set;}
+
+        public string Title { get; set; }
+
+        public string Description {get;set;}
+
+        public string Picture {get; set;}
+    }
+}
