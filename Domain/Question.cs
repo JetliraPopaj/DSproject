f50cbeb2983c53ecf1351ea3616e5e7398cb8d02
elmediNahmeti <ea52865@ubt-uni.net>
2023-02-05 23:07:15 +0100

    question cs added

---
+++ Domain/Question.cs
@@ -0,0 +1,20 @@
+namespace Domain
+{
+    public class Question 
+    {
+        public int Id{get;set;}
+        public string QuestionDescription { get; set; }
+        public string Option1{ get; set;}
+        public bool Answer1 { get; set; }
+        public string Option2{ get; set;}
+        public bool Answer2 { get; set; }
+        public string Option3{ get; set;}
+        public bool Answer3 { get; set; }
+        public string Option4{ get; set;}
+       
+        public bool Answer4 { get; set; }
+
+        
+
+    }
+}
