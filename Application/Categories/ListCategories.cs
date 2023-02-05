31d9fd605930ea668fbebe77e62e548cf90d4128
jetlirapopaj <jp48443@ubt-uni.net>
2023-02-05 20:32:01 +0100

    List of categories

---
+++ Application/Categories/ListCategories.cs
@@ -0,0 +1,29 @@
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Domain;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace Application.Categories
+{
+    public class ListCategories
+    {
+        public class Query : IRequest<List<Category>> {}
+
+        public class Handler : IRequestHandler<Query, List<Category>>
+        {
+            private readonly DataContext _context;
+            public Handler(DataContext context)
+            {
+                _context = context;
+
+            }
+            public async Task<List<Category>> Handle(Query request, CancellationToken cancellationToken)
+            {
+               return await _context.Categories.ToListAsync();
+            }
+        }
+    }
+}
